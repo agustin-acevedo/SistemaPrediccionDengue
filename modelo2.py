fb464f9f105bd46651dbd0121b4d8d715e9d1ae7
Agustin Acevedo <ac.nahuel.agustin@gmail.com>
2024-10-17 10:52:47 -0300

    Modelo final funcionando y graficos principales

--- modelo2.py
+++ modelo2.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Cargar el archivo CSV
 file_path = './DataSet/Dengue_Dataset.csv'
@@ -42,6 +44,15 @@ y = df_cleaned['casos_corrientes']
 # División en conjunto de entrenamiento y prueba (80% entrenamiento, 20% prueba)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+print("Xtrain")
+print(X_train)
+print("xtest: ")
+print(X_test)
+print("y_train")
+print(y_train)
+print("y_test")
+print(y_test)
+
 # Crear el modelo de regresión lineal
 model = LinearRegression()
 
@@ -50,54 +61,128 @@ model.fit(X_train, y_train)
 
 # Realizar predicciones con los datos de prueba
 y_pred = model.predict(X_test)
-
+print("Prediccion casos: ", y_pred)
 # Evaluar el modelo
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
 print(f'MAE: {mae}, R²: {rmse}')
 
-# Vamos a generar un rango de fechas futuras, manteniendo la periodicidad semanal
-# Seleccionamos la última fecha del dataset
-last_date = df_cleaned['fecha'].max()
+#---------------------------------------------------------------------------------------
 
-# Generamos las próximas 10 semanas
-future_dates = pd.date_range(start=last_date, periods=10, freq='W', name ='fecha')
+future_data = pd.read_csv('./clima_futuro.csv')
 
-# Para extrapolar los valores de precipitación y temperatura, calculamos el promedio semanal del dataset existente
-#mean_precipitation = df_cleaned['precipitacion_corrientes'].mean()
-#mean_temperature = df_cleaned['temperatura'].mean()
+# Mostrar las primeras filas del dataset para revisión
+print(future_data.head())
 
-mean_temp =  df_cleaned['temperatura'].mean()
-std_temp =  df_cleaned['temperatura'].std()
+# Convertir la columna 'fecha' al formato datetime
+future_data['fecha'] = pd.to_datetime(future_data['fecha'], format= '%Y-%m-%d')
 
-mean_precip =  df_cleaned['precipitacion_corrientes'].mean()
-std_precip =  df_cleaned['precipitacion_corrientes'].std()
+# Mostrar las primeras filas del dataset después del preprocesamiento
+print('Mostrar las primeras filas del dataset después del preprocesamiento')
+print(future_data.head())
 
-# # Crear un nuevo DataFrame con las fechas futuras y los valores extrapolados de precipitación y temperatura
-# future_data = pd.DataFrame({
-#     'fecha': future_dates,
-#     'temperatura': np.random.normal(mean_temp, std_temp, len(future_dates)),
-#     'precipitacion_corrientes': np.random.normal(mean_precip, std_precip, len(future_dates))
-# })
- #Creo un rango de fechas futuras (Tomando a partir de la ultima fecha que tiene el dataset)
-#future_dates = pd.date_range(start=last_date, periods=10, freq='W')
+# Seleccionar las columnas de interés para las predicciones futuras
+future_data = future_data[['fecha','precipitacion_corrientes', 'temperatura']]
+
+# Usar el modelo entrenado para predecir los casos de dengue en fechas futuras
+future_predictions = model.predict(future_data[['precipitacion_corrientes', 'temperatura']])
+future_predictions = future_predictions.astype(int)
+# Mostrar las predicciones
+print(future_predictions)
+
+# # # Añadir las predicciones a las fechas futuras y guardar en un archivo
+future_data['predicted_casos'] = future_predictions
+future_data.to_csv('predicciones_dengue_futuras.csv', index=False)
+
+#Realizo esto para que se pueda mostrar la cantidad de casos por mes, solo para que sea mas visible en los graficos 
+# Creo una columna adicional que solo contenga el año y mes en formato YYYY-MM
+df_cleaned['fecha_por_mes'] = df_cleaned['fecha'].dt.to_period('M')
+
+# Agrupar por 'fecha_por_mes' y sumar los casos de dengue por cada mes
+casos_por_mes = df_cleaned.groupby('fecha_por_mes')['casos_corrientes'].sum().reset_index()
+
+# Mostrar el resultado
+print(casos_por_mes)
+
+#REALIZO LO MISMO PARA LOS DATOS DE FECHAS FUTURAS
+future_data['fecha_por_mes'] = future_data['fecha'].dt.to_period('M')
+
+# Agrupar por 'fecha_por_mes' y sumar los casos de dengue por cada mes
+casos_por_mes_futuro = future_data.groupby('fecha_por_mes')['predicted_casos'].sum().reset_index()
+
+# Mostrar el resultado
+print(casos_por_mes_futuro)
 
 
-# Generar valores futuros usando una distribución normal alrededor de la media
-np.random.seed(42)  # Para resultados reproducibles
-future_data = pd.DataFrame({
-    'fecha': future_dates,
-    'temperatura': np.random.normal(mean_temp, std_temp, len(future_dates)),
-    'precipitacion_corrientes': np.random.normal(mean_precip, std_precip, len(future_dates))
-}, index=future_dates)
-# Con esto remplazo los valores negativos en la columna de precipitacion ya que no tiene sentido que sea negativo, los cambio por 0
-future_data['precipitacion_corrientes'] = future_data['precipitacion_corrientes'].apply(lambda x: max(x, 0))
+
+#import matplotlib.pyplot as plt
+#GRAFICA PARA VER LA CANTIDAD DE CASOS EN EL CONJUNTO DE DATOS HISTORICO
+# Graficar los casos de dengue por mes
+plt.figure(figsize=(15, 6))
+
+# Gráfico de líneas
+plt.plot(casos_por_mes['fecha_por_mes'].astype(str), casos_por_mes['casos_corrientes'], marker='o', linestyle='-', color='b', label='Casos por mes')
 
 
-# Realizar las predicciones utilizando el modelo entrenado
-X_future = future_data[['precipitacion_corrientes', 'temperatura']]
-future_data['predicted_casos_corrientes'] = model.predict(X_future)
+# Etiquetas y título
+plt.title('Cantidad de casos de Dengue por mes (YYYY-MM)')
+plt.xlabel('Fecha (YYYY-MM)',labelpad = 2)
+plt.ylabel('Cantidad de casos')
 
-# Mostrar las predicciones para las próximas 10 semanas
-print(future_data[['fecha', 'predicted_casos_corrientes']])
+# Rotar las etiquetas del eje x para mejorar la legibilidad
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom = 0.15) 
+
+# Añadir leyenda
+plt.legend()
+
+# Mostrar gráfico
+#plt.tight_layout()
+plt.show()
+
+#GRAFICA PARA VER LA CANTIDAD DE CASOS EN EL CONJUNTO DE DATOS HISTORICO
+# Graficar los casos de dengue por mes
+plt.figure(figsize=(15, 6))
+
+# Gráfico de líneas
+plt.plot(casos_por_mes_futuro['fecha_por_mes'].astype(str), casos_por_mes_futuro['predicted_casos'], marker='o', linestyle='-', color='b', label='Casos por mes')
+
+
+# Opcional: Gráfico de barras (puedes descomentar si prefieres barras)
+# plt.bar(casos_por_mes['fecha_por_mes'].astype(str), casos_por_mes['casos_corrientes'], color='skyblue')
+
+# Etiquetas y título
+plt.title('Cantidad de casos de Dengue por mes (YYYY-MM)')
+plt.xlabel('Fecha (YYYY-MM)',labelpad = 2)
+plt.ylabel('Cantidad de casos')
+
+# Rotar las etiquetas del eje x para mejorar la legibilidad
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom = 0.15) 
+
+# Añadir leyenda
+plt.legend()
+
+# Mostrar gráfico
+#plt.tight_layout()
+plt.show()
+
+# plt.figure(figsize=(14, 7))
+
+# # Gráfico para el conjunto inicial
+# plt.scatter(df_cleaned['temperatura'], df_cleaned['casos_corrientes'], color='blue', label='Datos históricos', alpha=0.5)
+# plt.scatter(df_cleaned['precipitacion_corrientes'], df_cleaned['casos_corrientes'], color='green', label='Datos históricos (Precipitación)', alpha=0.5)
+
+# # Gráfico para el conjunto futuro
+# plt.scatter(future_data['temperatura'], future_predictions, color='red', label='Predicciones futuras', alpha=0.5)
+# plt.scatter(future_data['precipitacion_corrientes'], future_predictions, color='orange', label='Predicciones futuras (Precipitación)', alpha=0.5)
+
+# # Añadir leyenda y etiquetas
+# plt.title('Comparación de casos de Dengue en base a Temperatura y Precipitación')
+# plt.xlabel('Valores de Temperatura y Precipitación')
+# plt.ylabel('Casos de Dengue')
+# plt.legend()
+
+# plt.tight_layout()
+# plt.show()
